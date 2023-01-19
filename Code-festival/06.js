91523ee8bbd33996bd9eaade2b205f502a05ccde
Juram Lee <juram2@naver.com>
2023-01-20 00:36:00 +0900

    code-festival 1~11번 문제 풀이

---
+++ Code-festival/06.js
@@ -0,0 +1,14 @@
+// False
+// 다음은 자바스크립트 문법 중에서 False로 취급하는 것들입니다. 앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요
+
+let q1 = NaN;
+let q2 = 1;
+let q3 = "";
+let q4 = 0;
+let q5 = undefined;
+
+console.log(Boolean(q2));
+
+
+// 예상 답 : 2번 / true는 1이다.
+// 실제 답 : 2번
