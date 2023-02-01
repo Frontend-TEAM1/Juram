1b5c8f5cc48383d4c0d1c207805a24a6622f2fc4
Juram Lee <juram2@naver.com>
2023-02-01 19:42:54 +0900

    disk.html 업로드

--- Personal/disk/disk.js
+++ Personal/disk/disk.js
@@ -1,25 +1,150 @@
+//====================================================DATA==================================================
 const musicListData = [
-    {
-      src: "./assets/image/iu_0.jpg",
-      color: ["#0272a4", "#f6a564"],
-    },
-    {
-      src: "./assets/image/iu_1.jpg",
-      color: ["#b6bfc8", "#36595b"],
-    },
-    {
-      src: "./assets/image/iu_2.jpg",
-      color: ["#e58e82", "#6f569f"],
-    },
-    
-  ];
-  
-  
-  
-  
-  
-  
-  /* 요소정리
+  {
+    src: "./assets/iu_0.jpg",
+    color: ["#0272a4", "#f6a564"],
+  },
+  {
+    src: "./assets/iu_1.jpg",
+    color: ["#b6bfc8", "#36595b"],
+  },
+  {
+    src: "./assets/iu_2.jpg",
+    color: ["#e58e82", "#6f569f"],
+  },
+];
+
+//===========================================================================================================
+// 비함수(변수, DOM API, 즉시실행문)
+let currentIndex = 0;
+let playStatus = false;
+let selected = false;
+const $album = document.querySelector("#ls");
+const songs = $album.children;
+const $diskInner = document.querySelector(".disk_inner");
+const $main = document.getElementById("main");
+const $playBtn = document.querySelector('.play_btn_group > button:first-child');
+const $stopBtn = document.querySelector('.play_btn_group > button:last-child');
+const $prev = document.querySelectorAll(".btns")[0];
+const $next = document.querySelectorAll(".btns")[1];
+const $disk = document.querySelector('.disk');
+
+
+// 이미지asset 넣기
+for (let i = 0; i < musicListData.length; i++) {
+  const $song = document.createElement("li");
+  $song.innerHTML = `
+  <img src=${musicListData[i].src} />
+  `;
+  $song.addEventListener("click", selectImg);
+  $album.append($song);
+}
+
+//===========================================================================================================
+// 함수들
+
+
+// 이미지에 css효과 붙이기 + 이미지 클릭이벤트로도 이동가능 + disk inner색상과 bg색상도 변경(bgUpdate()함수사용)
+function selectImg(event) {
+  for (el of songs) {
+    if (event.target.parentElement === el) {
+      el.classList.add("play");
+      currentIndex = [...songs].findIndex((item) => item == this);
+      selected = true;
+      bgUpdate();
+    } else el.classList.remove("play");
+  }
+
+  // for in으로 했을때는 왜 remove에 에러가 났을까?
+  // for (el in songs) {
+  //   if (event.target.parentElement === songs[el]) {
+  //     songs[el].classList.add("play");
+  //     currentIndex = [...songs].findIndex((item) => item == this);
+  //     selected = true;
+  //     bgUpdate();
+  //   } else songs[el].classList.remove("play");   
+  // }
+}
+
+
+// 배경 바꾸기
+function bgUpdate() {
+  $diskInner.setAttribute(
+    "style",
+    `background-color: ${musicListData[currentIndex].color[0]}`
+  );
+  $main.setAttribute(
+    "style",
+    `background: linear-gradient(120deg, ${musicListData[currentIndex].color[0]}, ${musicListData[currentIndex].color[1]})`
+  );
+
+  // Play시 배경이 앨범 이미지로 바뀌는건 구현 못함.. 어디에 접근해야할까?
+  // if( selected === true && playStatus === true) {
+  //   // $main.style.backgroundImage = `url('${musicListData[currentIndex].src}')`;
+  //   $main.setAttribute(
+  //     "style",
+  //     `background: src('${musicListData[currentIndex].src})`
+  //   );
+  // }
+}
+
+
+// 좌우 버튼 눌렸을 때 이동하기
+$prev.addEventListener("click", prevPlay);
+$next.addEventListener("click", nextPlay);
+
+function prevPlay() {
+  if (currentIndex <= 0) {
+    currentIndex = 0;
+  } else currentIndex--;
+  bgUpdate();
+  for (let i = 0; i < musicListData.length; i++) {
+    songs[i].classList.remove("play");
+  }
+
+  selectedMusic();
+}
+
+function nextPlay() {
+  if (currentIndex >= musicListData.length - 1) {
+    currentIndex = 0;
+  } else currentIndex++;
+  bgUpdate();
+  for (let i = 0; i < musicListData.length; i++) {
+    songs[i].classList.remove("play");
+  }
+
+  selectedMusic();
+}
+
+// 선택된 음악표시
+function selectedMusic() {
+  selected = true;
+  songs[currentIndex].classList.add("play");
+  bgUpdate();
+}
+
+// 음악 재생, 음악 멈추기
+$playBtn.addEventListener('click', playMusic);
+$stopBtn.addEventListener('click', stopMusic);
+
+function playMusic() {
+  playStatus = true;
+  if(selected == true){
+  $disk.classList.add('active');
+  $main.children[0].src = musicListData[currentIndex].src;
+  }
+}
+
+function stopMusic() {
+  playStatus = false;
+  $main.children[0].src = ""
+  $disk.classList.remove('active');
+}
+
+
+
+/* 요소정리
   1. .list_btn_group > ul
       - li태그의 자식요소로 이미지를 갖고
       - 해당 ul의 자식으로서 추가
@@ -51,4 +176,3 @@ const musicListData = [
   
   8. 이미지 눌렀을 때도 이동 가능(재생 중일 때 클릭되면 재생 노래도 변경)
   */
-  
